147a973d3a2370d923e6ec034c4caa57bdd4615d
David luque Alba <dvdlq99@gmail.com>
2021-01-26 20:33:33 +0100

    upload

---
+++ ts-fast-crash/example.js
@@ -0,0 +1,18 @@
+var Example = /** @class */ (function () {
+    function Example() {
+        this.house = null;
+    }
+    Example.prototype.run = function () {
+        alert('Hello');
+    };
+    ;
+    return Example;
+}());
+;
+// tsc -w <filename> => to set "watch mode" and compile typescript automatically
+var Awesome = /** @class */ (function () {
+    function Awesome() {
+    }
+    return Awesome;
+}());
+;
---
+++ ts-fast-crash/example.ts
@@ -0,0 +1,14 @@
+
+class Example {
+    public house = null;
+    run(){
+        alert('Hello');
+    };
+};
+
+// tsc -w <filename> => to set "watch mode" and compile typescript automatically
+
+class Awesome {
+    name : string
+};
+
